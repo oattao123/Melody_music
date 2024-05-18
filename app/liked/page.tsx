b7515025a76de22e54f3032deee33c07fb74a366
Ptrchn_deaw <deawandnathai@gmail.com>
2024-05-18 14:00:27 +0700

    Update page.tsx

--- app/liked/page.tsx
+++ app/liked/page.tsx
@@ -36,7 +36,7 @@ const Liked = async () => {
               <Image
                 className="object-cover"
                 fill
-                src="/images/liked.png"
+                src="/images/playlist_icon.png"
                 alt="Playlist"
               />
             </div>
